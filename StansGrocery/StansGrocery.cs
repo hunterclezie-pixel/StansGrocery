bb75f72196d3010ed428a06a33564a1726a472d6
Hunter Clezie <clezhunt@gmail.com>
2026-04-27 16:12:23 -0600

    add header

---
+++ StansGrocery/StansGrocery.cs
@@ -0,0 +1,17 @@
+/* 
+Hunter Clezie 
+Spring 2026
+RCET2265
+StansGrocery
+github url: https://github.com/hunterclezie-pixel/StansGrocery.git
+*/
+namespace StansGrocery
+{
+    public partial class StansGrocery : Form
+    {
+        public StansGrocery()
+        {
+            InitializeComponent();
+        }
+    }
+}
