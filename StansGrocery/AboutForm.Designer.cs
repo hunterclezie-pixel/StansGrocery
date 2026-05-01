d4c8ae2326f5f89b03f6ad15d8e0c34ecc611fa6
Hunter Clezie <clezhunt@gmail.com>
2026-05-01 11:36:33 -0600

    add about form

---
+++ StansGrocery/AboutForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace StansGrocery
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AboutLabel = new Label();
+            OkButton = new Button();
+            SuspendLayout();
+            // 
+            // AboutLabel
+            // 
+            AboutLabel.Location = new Point(12, 9);
+            AboutLabel.Name = "AboutLabel";
+            AboutLabel.Size = new Size(331, 203);
+            AboutLabel.TabIndex = 0;
+            AboutLabel.Text = "About Label";
+            // 
+            // OkButton
+            // 
+            OkButton.Location = new Point(249, 226);
+            OkButton.Name = "OkButton";
+            OkButton.Size = new Size(94, 39);
+            OkButton.TabIndex = 1;
+            OkButton.Text = "Ok";
+            OkButton.UseVisualStyleBackColor = true;
+            // 
+            // AboutForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(355, 277);
+            Controls.Add(OkButton);
+            Controls.Add(AboutLabel);
+            Name = "AboutForm";
+            Text = "AboutForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label AboutLabel;
+        private Button OkButton;
+    }
+}
---
+++ StansGrocery/AboutForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StansGrocery
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+            UpdateLabel();
+        }
+
+        void UpdateLabel()
+        {
+            AboutLabel.Text = $"Welcome to StansGrocery!" + "\n" +
+            $"This program is designed to help you manage your grocery store efficiently." + "\n" +
+            $"You can sort by either the aisle or the category of food." + "\n" +
+            $"You can also use the search feature to quickly find specific items." + "\n" +
+            $"Thank you for using StansGrocery! Press the \"OK\" button to continue.";
+        }
+    }
+}
